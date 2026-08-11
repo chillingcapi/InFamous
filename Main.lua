0e736c600f102278fea63f9f38e307c26ea2a462
chillingcapi <chillingcapy3@gmail.com>
2026-08-11 23:36:06 +0200

    Run the loading screen before luarmor

--- Main.lua
+++ Main.lua
@@ -1,1 +1,5 @@
+pcall(function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/chillingcapi/InFamous/refs/heads/main/Loading.lua"))()
+end)
+
 loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/9bb63babcc39e4ca6c45fda7041b9ab0.lua"))()
