2cbfea58edefd2df3e05da89f2c4c51ed1a444b4
chillingcapi <chillingcapy3@gmail.com>
2026-08-14 21:16:19 +0200

    Create Public Main.lua

---
+++ Public Main.lua
@@ -0,0 +1,7 @@
+-- public
+
+pcall(function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/chillingcapi/InFamous/refs/heads/main/Loading.lua"))()
+end)
+
+loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/bbb9e167f6d5801e97294a2b01dd1fa6.lua"))()
